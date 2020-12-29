c86c6cfe759bb42f82d94973aeff94053d2898f7
newsha <nioosha.h@gmail.com>
2020-12-29 17:24:50 +0330

    added find object in distance.js and an api key I received from zipcodeapi.com. also, added a function for handling request response

--- api/routes.js
+++ api/routes.js
@@ -6,3 +6,5 @@ module.exports = function (app) {
   app.route("/about").get(controller.about);
   app.route("/distance/:zipcode1/:zipcode2").get(controller.getDistance);
 };
+
+l8V6jQhwsO9XMsryFp8le54HAeCm3FtRqlfuYYOQ8q7GRVgh5rsGeNiWEi0WJC1B;
